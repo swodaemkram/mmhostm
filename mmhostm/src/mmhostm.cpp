6a1aebf7e550caa1d5c25cc90cc51bbb86453839
mark <mark@mark-NUC7i5BNK>
2019-11-11 08:17:17 -0500

    First Commit

---
+++ mmhostm/src/mmhostm.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : mmhostm.cpp
+// Author      : Mark Meadows
+// Version     :
+// Copyright   : Copyright (c) 2019 Mark Meadows
+// Description : Mattermost Host Monitor in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+
+
+	cout << "Mattermost Host Monitor" << endl; // Mattermost Host Monitor
+	return 0;
+}
+
+
+
+
